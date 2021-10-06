145c353680c4f675bea980fe2714be8747c442ce
Anurag Yadav <anuragyadavmnp@gmail.com>
2021-10-06 23:04:45 +0530

    Add Alternating Sequence Solution

---
+++ Uni Assignment CUPA2101/alternating_subsequence.py
@@ -0,0 +1,14 @@
+#https://www.codechef.com/CUPA2101/problems/CHEFALT
+#Anurag Yadav 20BCS5427
+
+T = int(input())
+while(T>0):
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    max = max(A)
+    for i in range(0,N){
+        for j in range(i,N){
+            if()
+        }
+    }
