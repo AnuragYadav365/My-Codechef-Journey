128f6029b0bd92ff5788e1709f61c038d4c115fe
Anurag Yadav <anuragyadavmnp@gmail.com>
2021-10-16 06:12:29 +0530

    Add snackdown 2021 q1 lucky number

---
+++ Snackdown 2021/Qualifying Round1/Lucky_number.py
@@ -0,0 +1,9 @@
+#https://www.codechef.com/SNCKQL21/problems/LUCKYNUM
+#Anurag Yadav
+#https://github.com/AnuragYadav365
+
+for _ in range(int(input())):
+    if 7 in list(map(int, input().split())):
+        print("YES")
+    else:
+        print("NO")
